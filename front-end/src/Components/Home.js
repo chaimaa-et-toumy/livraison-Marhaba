128a4be4f9de8bf1c897ba6a0ebccc329dd1146f
chaimaa et-toumy <chaimaetoumy5@gmail.com>
2022-11-15 17:45:28 +0100

    dry navbar

--- front-end/src/Components/Home.js
+++ front-end/src/Components/Home.js
@@ -1,28 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Navbar from "./childCompenent/Navbar";
 
 function Home() {
     return (
       <div className="wrapper" style={{backgroundImage: `url('images/bg-registration-form-1.jpg')`}}>
         {
-        <nav className="navbar navbar-expand-lg navbar-light fixed-top">
-            <div className="container">
-            <Link className="navbar-brand fw-bold" to={'/'}>Marhaba</Link>
-                <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
-                    <ul className="navbar-nav ml-auto" style={{fontSize : "18px"}}>
-                        <li className="nav-item">
-                          <Link className="nav-link" to={'/'}>Home</Link>
-                        </li>
-                        <li className="nav-item">
-                          <Link className="nav-link" to={'/Login'}>Login</Link>
-                        </li>
-                        <li className="nav-item">
-                          <Link className="nav-link" to={'/Register'}>Register</Link>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
+        <Navbar/>
         }
         <div>
           <h2 className="fs-4 text-center d-flex justify-content-center">Welcome to our Application Marhaba</h2>
--- front-end/src/Components/childCompenent/Navbar.js
+++ front-end/src/Components/childCompenent/Navbar.js
@@ -2,22 +2,43 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 function Navbar() {
+    const token_ = localStorage.getItem('token')
   return (
     <nav className="navbar navbar-expand-lg navbar-light fixed-top">
-        <div className="container">
-            <Link className="navbar-brand fw-bold" to={'/'}>Marhaba</Link>
-            <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+    <div className="container">
+    <Link className="navbar-brand fw-bold" to={'/'}>Marhaba</Link>
+        <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+            <ul className="navbar-nav ml-auto" style={{fontSize : "18px"}}>
+                <li className="nav-item">
+                  <Link className="nav-link" to={'/'}>Home</Link>
+                </li>
+                {
+                // condition token
+                  token_ ? <li className='nav-item'>
+                  <Link className="nav-link fw-bold" to={'/logout'}>Logout</Link>
+                </li> : 
                 <ul className="navbar-nav ml-auto" style={{fontSize : "18px"}}>
-                    <li className="nav-item">
-                        <Link className="nav-link fw-bold" to={'/'}>Home</Link>
-                    </li>
-                    <li className=''>
-                        <Link className="nav-link fw-bold" to={'/logout'}>Logout</Link>
-                    </li>
+                  <li className="nav-item">
+                  <Link className="nav-link" to={'/Login'}>Login</Link>
+                </li>
+                  <li className="nav-item">
+                  <Link className="nav-link" to={'/Register'}>Register</Link>
+                  </li>
                 </ul>
-            </div>
+                }
+                {/* <li className="nav-item">
+                  <Link className="nav-link" to={'/Login'}>Login</Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to={'/Register'}>Register</Link>
+                </li>
+                <li className='nav-item'>
+                  <Link className="nav-link fw-bold" to={'/logout'}>Logout</Link>
+                </li> */}
+            </ul>
         </div>
-    </nav>
+    </div>
+</nav>
   )
 }
 
